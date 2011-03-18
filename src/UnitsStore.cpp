jerryshu/xcsoar

commit d7c10fc652ea10dd18fce541af5398b81826b123

UnitsStore: Fixed builtin Units templates
Something got mixed up apparently...

This fixed ticket #999 ;)

src/UnitsStore.cpp
@@ -42,7 +42,7 @@ static const UnitStoreItem Presets[] =
     unKiloMeterPerHour
   } },
   { _T("British"), {
-    unKiloMeter,
+    unNauticalMiles,
     unFeet,
     unGradCelcius,
     unKnots,
@@ -60,13 +60,13 @@ static const UnitStoreItem Presets[] =
     unStatuteMilesPerHour
   } },
   { _T("Australian"), {
-    unNauticalMiles,
-    unMeter,
+    unKiloMeter,
+    unFeet,
     unGradCelcius,
     unKnots,
     unKnots,
     unKnots,
-    unKnots
+    unKiloMeterPerHour
   } }
 };
 

commit d9c6aa696866e0af89ad64b621c3b14750f90f64

UnitStore: Fix British defaults
Corrects an error introduced by d7c10fc65.

src/UnitsStore.cpp
@@ -42,7 +42,7 @@ static const UnitStoreItem Presets[] =
     unKiloMeterPerHour
   } },
   { _T("British"), {
-    unNauticalMiles,
+    unKiloMeter,
     unFeet,
     unGradCelcius,
     unKnots,